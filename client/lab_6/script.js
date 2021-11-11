56c7f54aa363472478f6ddffdcd783216217b8c7
des919 <81268956+des919@users.noreply.github.com>
2021-11-11 00:40:01 -0500

    Lab 6

---
+++ client/lab_6/script.js
@@ -0,0 +1,77 @@
+/* Document example */
+// document.addEventListener('click', (event) => {
+//   console.log('click registered', event.target);
+// });
+
+async function fetchRequest(url) {
+    try {
+    const request = await fetch(url);
+    const json = await request.json();
+    console.table(json);
+    return json;
+}   catch(err)
+ { console.error(err);
+    return err;
+}}
+
+async function mainThread() {
+    console.log('loaded main script');
+    const url = 'https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json';
+    const inputBox = document.querySelector('#Zipcode');
+    const target = document.querySelector('.suggestions');
+
+    /* Actually linked script example */
+    const targetElement = document.querySelector('.click_demo');
+    // targetElement.addEventListener('click', (event) => runMeOnClickEvent(event));
+
+    const data = await fetchRequest(url);
+    console.log('external dataset', data);
+
+    inputBox.addEventListener('change', (event) => {
+        display(event, data);
+        
+        });
+
+    inputBox.addEventListener('keyup', (event) => {
+        display(event, data);
+        
+        });
+
+    
+
+
+    function findMatches(wordToMatch, cities) {
+        return cities.filter(place => {
+            const regex = new RegExp(wordToMatch, 'gi');
+            return place.name.match(regex) || place.category.match(regex) ||
+            place.city.match(regex) || place.zip.match(regex) || place.address_line_1.match(regex)
+        })
+    }
+
+    function display(event, data) {
+        const matchArray = findMatches(event.target.value, data);
+        console.log(matchArray)
+        const html = matchArray.map(place => {
+           return `
+            <li>
+                <span class="name">${place.name}</span>
+
+                <span class="name">${place.category}</span>
+            
+                <span class="name">${place.city}</span>
+            
+                <span class="name">${place.zip}</span>
+
+                <span class="name">${place.address_line_1}</span>
+            </li>
+        `})
+        target.innerHTML = html
+    }
+};
+window.onload = mainThread;
+
+// const fetchElement = document.querySelector('.fetch');
+//     fetchElement.addEventListener('click', async (event) => {
+//         console.log('dataset size from county', data.length);
+//         console.log('displaySet contents', dataset.length);
+//     });
