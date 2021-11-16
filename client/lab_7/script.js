fee6e3521edc1225f4794c794d360f463cd00a5a
des919 <81268956+des919@users.noreply.github.com>
2021-11-16 18:20:08 -0500

    Lab 7

--- client/lab_7/script.js
+++ client/lab_7/script.js
@@ -7,7 +7,6 @@ async function fetchRequest(url) {
     try {
     const request = await fetch(url);
     const json = await request.json();
-    console.table(json);
     return json;
 }   catch(err)
  { console.error(err);
@@ -19,7 +18,7 @@ async function mainThread() {
     const url = 'https://data.princegeorgescountymd.gov/resource/umjn-t2iz.json';
     const inputBox = document.querySelector('#Zipcode');
     const target = document.querySelector('.suggestions');
-    const mymap = L.map('map').setView([51.505, -0.09], 13);
+    const mymap = L.map('map').setView([38.989, -76.93], 10);
     L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
         attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
         maxZoom: 18,
@@ -46,31 +45,32 @@ async function mainThread() {
         
         });
 
-    
-
-
-    function findMatches(wordToMatch, cities) {
-        return cities.filter(place => {
-            const regex = new RegExp(wordToMatch, 'gi');
-            return place.name.match(regex) || place.category.match(regex) ||
-            place.city.match(regex) || place.zip.match(regex) || place.address_line_1.match(regex)
+    function findMatches(wordToMatch, mymap, cities) {
+        mymap.eachLayer((layer) => {
+    if (layer._latlng !== undefined) { layer.remove(); }
+  //   mymap.setView([0,0],0)
+  });
+        console.log(wordToMatch)
+        const filterList = cities.filter(place => {
+            const zipcode = wordToMatch
+            return place.zip === zipcode
         })
+        return filterList
     }
 
     function display(event, data) {
-        const matchArray = findMatches(event.target.value, data);
-        console.log(matchArray)
-        const html = matchArray.map(place => {
+        const matchArray = findMatches(event.target.value, mymap, data);
+        const newList = matchArray.slice(0,5)
+        console.log(newList)
+        newList.forEach((item)=>{
+            const point = item.geocoded_column_1
+            const coords = point.coordinates.reverse()
+            L.marker(coords).addTo(mymap)
+        })
+        const html = newList.map(place => {
            return `
             <li>
                 <span class="name">${place.name}</span><br>
-
-                <span class="name">${place.category}</span><br>
-            
-                <span class="name">${place.city}</span><br>
-            
-                <span class="name">${place.zip}</span><br>
-
                 <span class="name">${place.address_line_1}</span><br>
             </li>
         `})
@@ -79,8 +79,7 @@ async function mainThread() {
 };
 window.onload = mainThread;
 
-
-var marker = L.marker([51.5, -0.09]).addTo(mymap);
+// var marker = L.marker([51.5, -0.09]).addTo(mymap);
 
 
 // const fetchElement = document.querySelector('.fetch');
